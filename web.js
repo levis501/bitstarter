3592892a5fc6a2195598b2031049c995d2e9f678
Ubuntu <ubuntu@ip-10-202-65-40.ec2.internal>
2013-07-10 15:57:30 +0000

    Added missing require for fs in web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var text = fs.readFileSync("index.html").toString();
 
 app.get('/', function(request, response) {
