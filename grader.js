9d2c43f3fd09fc563059641964cf05c277320ed5
Ubuntu <ubuntu@ip-10-202-65-40.ec2.internal>
2013-07-11 06:16:42 +0000

    Added restler.  Added to checks.json.  Added url to grader.js

--- grader.js
+++ grader.js
@@ -3,8 +3,10 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = false;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -14,6 +16,35 @@ var assertFileExists = function(infile) {
 	}
     return instr;
 };
+
+var assertURLExists = function(inURL) {
+    return inURL;
+}
+
+var buildfn = function(csvfile, headers) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", csvfile);
+            fs.writeFileSync(csvfile, result);
+            csv2console(csvfile, headers);
+        }
+    };
+    return response2console;
+};
+
+var marketResearch = function(symbols, columns, csvfile, headers) {
+    symbols = symbols || SYMBOLS_DEFAULT;
+    columns = columns || COLUMNS_DEFAULT;
+    csvfile = csvfile || CSVFILE_DEFAULT;
+    headers = headers || HEADERS_DEFAULT;
+    var apiurl = financeurl(symbols, columns);
+    var response2console = buildfn(csvfile, headers);
+    rest.get(apiurl).on('complete', response2console);
+};
+
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -34,6 +65,24 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURLCallback = function(checksfile) {
+    var callback = function(result, response) {
+	$ = cheerio.load(result);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	}
+	console.log(JSON.stringify(out, null, 4));
+    };
+    return callback;
+};
+
+var checkURL = function(url, checksfile) {
+    rest.get(url).on('complete', checkURLCallback(checksfile));
+}
+
 var clone = function(fn) {
     // workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -43,10 +92,16 @@ var clone = function(fn) {
 if(require.main == module) {
     program.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'URL to html', clone(assertURLExists), URL_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+	var checkJson = checkURL(program.url, program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
